947b21dcb42537b1acf20a7c8aefd9dbd030f409
SantiagoDuarte99 <josesantiagoddua@ufps.edu.co>
2026-04-16 08:47:15 -0500

    refactor: registrar controladores cursos y actividades en modulo aprendiz

--- internal/bari_lms/controllers/aprendiz/__init__.py
+++ internal/bari_lms/controllers/aprendiz/__init__.py
@@ -1,10 +1,9 @@
 """Controladores del módulo aprendiz."""
 
-from bari_lms.controllers.aprendiz import calificaciones, evidencias, fichas, perfil
+from bari_lms.controllers.aprendiz import actividades, cursos, perfil
 
 
 def register_routes(app):
-    fichas.register_routes(app)
-    calificaciones.register_routes(app)
-    evidencias.register_routes(app)
+    cursos.register_routes(app)
+    actividades.register_routes(app)
     perfil.register_routes(app)
